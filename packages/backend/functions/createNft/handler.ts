f0df4804a4312252ab04a715a5acf9d328e3f5e6
Theo Leonard <theole@theodo.fr>
2024-03-13 11:54:04 +0100

    Refactored createNft

--- packages/backend/functions/createNft/handler.ts
+++ packages/backend/functions/createNft/handler.ts
@@ -6,34 +6,57 @@ const randomIntFromInterval = (min: number, max: number) => {
     return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
-export const main = async (event: { pathParameters: { ownerUserId: string }}): Promise<any> => {
+const createUser = async (userId: string, nftPrice: number) => {
+    const user = {
+        PK: 'User',
+        SK: userId,
+        balance: 1000 - nftPrice,
+    }
+    await UserEntity.put(user);
+}
+
+const createNewNft = async (userId: string) => {
     const id = randomUUID();
 
     const Item = {
         SK: id,
         id: id,
-        ownerUserId: event.pathParameters.ownerUserId,
+        ownerUserId: userId,
         price: randomIntFromInterval(1, 100),
         positionX: randomIntFromInterval(5, 90),
         positionY: randomIntFromInterval(10, 90),
         imageIndex: Math.floor(Math.random() * 5),
     }
 
-        
-    let User = await UserEntity.get({PK: 'User', SK: event.pathParameters.ownerUserId});
-    if (!User.Item) {
-        const user = {
-            PK: 'User',
-            SK: event.pathParameters.ownerUserId,
-            balance: 1000 - Item.price,
-        }
+    await NFTEntity.put(Item);
+    return Item
+}
+
+const getUser = async (userId: string) => {
+    return (await UserEntity.get({PK: 'User', SK: userId})).Item;
+}
+
+const updateUserBalance = async (userId:string, price: number) => {
+    let user = await getUser(userId)
+    // Create User if not exists
+    if (!user) {
+        await createUser(userId, price)
+    }
+    //update user if exists
+    else if(user.balance) {
+        user.balance -= price;
         await UserEntity.put(user);
     }
-    else if(User.Item.balance) {
-        User.Item.balance -= Item.price;
-        await UserEntity.put(User.Item);
-    }
+}
 
-    await NFTEntity.put(Item);
-    return Item;
+
+export const main = async (event: { pathParameters: { ownerUserId: string }}): Promise<any> => {
+
+    // Create new NFT with ownerUserId
+    const nft = await createNewNft(event.pathParameters.ownerUserId)
+
+    // Update User balance
+    await updateUserBalance(event.pathParameters.ownerUserId, nft.price)
+
+    return nft;
 };
