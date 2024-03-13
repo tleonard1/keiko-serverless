c88142593711d44fe04018c1ac6e0cfaa46a013e
Theo Leonard <theole@theodo.fr>
2024-03-13 12:11:03 +0100

    Refactored deleteNft

--- packages/backend/functions/deleteNft/handler.ts
+++ packages/backend/functions/deleteNft/handler.ts
@@ -1,20 +1,31 @@
 import { NFTEntity } from "libs/dynamodb-toolbox/nftEntity";
-import {UserEntity } from "libs/dynamodb-toolbox/userEntity";
+import { UserEntity } from "libs/dynamodb-toolbox/userEntity";
 
-export const main = async (event: { pathParameters: { id: string }}): Promise<any> => {
 
-    let Nft = await NFTEntity.get({PK: 'Nft', SK: event.pathParameters.id});
-    if (!Nft.Item) {
+const getNft = async (id: string) => {
+    return (await NFTEntity.get({PK: 'Nft', SK: id})).Item
+}
+
+const getUser = async (userId: string) => {
+    return (await UserEntity.get({PK: 'User', SK: userId})).Item;
+}
+
+const updateUserBalance = async (userId: string, price: number) => {
+    const user = await getUser(userId)
+    if (user?.balance) {
+        user.balance += price ?? 0;
+        await UserEntity.put(user);
+    }
+}
+
+export const main = async (event: { pathParameters: { id: string }}): Promise<void> => {
+
+    const nft = await getNft(event.pathParameters.id)
+    if (!nft) {
         throw new Error('Nft not found');
     }
-    else if(Nft.Item.ownerUserId) {
-        const User = (await UserEntity.get({PK: 'User', SK: Nft.Item.ownerUserId})).Item;
-        if (User?.balance) {
-            User.balance += Nft.Item.price ?? 0;
-            await UserEntity.put(User);
-        }
-    }
 
-    const response = await NFTEntity.delete({SK: event.pathParameters.id});
-    return response;
+    await updateUserBalance(nft.ownerUserId ?? "", nft.price ?? 0);
+    
+    await NFTEntity.delete({SK: event.pathParameters.id});
 };
